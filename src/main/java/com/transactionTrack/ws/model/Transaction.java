1d81900c8fd28086a57c29b8beb6f17f2fd281cf
berzanozgenc <berzanozgenc@gmail.com>
2024-07-22 13:53:11 +0300

    Swagger Configs updated

--- src/main/java/com/transactionTrack/ws/model/Transaction.java
+++ src/main/java/com/transactionTrack/ws/model/Transaction.java
@@ -3,11 +3,13 @@ package com.transactionTrack.ws.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Table
+@Schema
 @Entity
 @Getter
 @Setter
@@ -15,15 +17,19 @@ public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of the transaction", example = "1")
     private Long id;
 
     @Column(nullable = false)
+    @Schema(description = " Amount of the transaction", example = "250")
     private BigDecimal amount;
 
     @Column(nullable = false)
+    @Schema(description = "Date of the transaction")
     private LocalDateTime transactionDate;
 
     @ManyToOne
+    @Schema(description = "User of the transaction")
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
--- src/main/java/com/transactionTrack/ws/model/User.java
+++ src/main/java/com/transactionTrack/ws/model/User.java
@@ -1,10 +1,12 @@
 package com.transactionTrack.ws.model;
 
 import jakarta.persistence.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Table(name="TransactionTrack_User")
+@Schema
 @Entity
 @Setter
 @Getter
