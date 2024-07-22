5433ba38edec7925f9870f9db31fbac17047c226
berzanozgenc <berzanozgenc@gmail.com>
2024-07-22 03:37:56 +0300

    EnableScheduling annotation added

--- src/main/java/com/transactionTrack/ws/WsApplication.java
+++ src/main/java/com/transactionTrack/ws/WsApplication.java
@@ -2,8 +2,10 @@ package com.transactionTrack.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WsApplication {
 
 	public static void main(String[] args) {
